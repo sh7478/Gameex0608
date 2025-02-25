5cf5ba412c1e0563870f7f006d207b12e8a945dc
sh7478 <sh7478@bs.amalnet.k12.il>
2025-02-25 18:22:10 +0200

    fix text on design and start of java code

--- app/src/main/java/com/example/gameex0608/MainActivity.java
+++ app/src/main/java/com/example/gameex0608/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.gameex0608;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +15,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnNew;
+    Button btnSend1;
+    Button btnSend2;
+    Button btnSend3;
+    LinearLayout firstCol;
+    LinearLayout secondCol;
+    LinearLayout thirdCol;
+    TextView txtVNum1;
+    TextView txtVNum2;
+    TextView txtVNum3;
+    TextView txtVNum4;
+    TextView txtVSum1;
+    TextView txtVSum2;
+    ImageView firstMark;
+    ImageView secondMark;
+    ImageView thirdMark;
+    EditText firstAns;
+    EditText secondAns;
+    EditText thirdAns;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
