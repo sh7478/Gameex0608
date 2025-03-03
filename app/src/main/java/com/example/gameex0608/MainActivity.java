2675cd22b0f64fbf2acdb44b9fb3f3c495f56ead
sh7478 <sh7478@bs.amalnet.k12.il>
2025-03-03 12:06:15 +0200

    fix bugs, add images.

--- app/src/main/java/com/example/gameex0608/MainActivity.java
+++ app/src/main/java/com/example/gameex0608/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.gameex0608;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -34,9 +35,76 @@ public class MainActivity extends AppCompatActivity {
     EditText firstAns;
     EditText secondAns;
     EditText thirdAns;
+    int answer;
+    int sum;
+    int num1;
+    int num2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnNew = findViewById(R.id.btnNew);
+        btnSend1=findViewById(R.id.btnSend1);
+        btnSend2=findViewById(R.id.btnSend2);
+        btnSend3=findViewById(R.id.btnSend3);
+        firstCol=findViewById(R.id.firstCol);
+        secondCol=findViewById(R.id.secondCol);
+        thirdCol=findViewById(R.id.thirdCol);
+        txtVNum1=findViewById(R.id.txtVNum1);
+        txtVNum2=findViewById(R.id.txtVNum2);
+        txtVNum3=findViewById(R.id.txtVNum3);
+        txtVNum4=findViewById(R.id.txtVNum4);
+        txtVSum1=findViewById(R.id.txtVSum1);
+        txtVSum2=findViewById(R.id.txtVSum2);
+        firstMark=findViewById(R.id.firstMark);
+        secondMark=findViewById(R.id.secondMark);
+        thirdMark=findViewById(R.id.thirdMark);
+        firstAns=findViewById(R.id.firstAns);
+        secondAns=findViewById(R.id.secondAns);
+        thirdAns=findViewById(R.id.thirdAns);
+        startGame();
+    }
+
+    public void firstSent(View view)
+    {
+        continueGame();
+    }
+    public void secondSent(View view)
+    {
+        continueGame();
+    }
+
+    public void newGame(View view)
+    {
+        startGame();
+    }
+    public void continueGame()
+    {
+        if(answer == sum)
+        {
+            firstMark.setImageResource(R.drawable.correct);
+        }
+        else
+        {
+            firstMark.setImageResource(R.drawable.wrong);
+        }
+        secondCol.setVisibility(View.VISIBLE);
+    }
+
+    public void startGame()
+    {
+        secondCol.setVisibility(View.INVISIBLE);
+        thirdCol.setVisibility(View.INVISIBLE);
+        btnSend1.setVisibility(View.INVISIBLE);
+        answer = 0;
+        sum = 0;
+        num1 = 0;
+        num2 = 0;
+        txtVNum1.setText("");
+        txtVNum2.setText("");
+        txtVNum3.setText("");
+        txtVNum4.setText("");
+        txtVSum1.setText("");
+        txtVSum2.setText("");
     }
 }
